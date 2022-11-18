f7a80e505e85334b0eafb16e5dfbe05df118f4fb
sjg20 <sjg20@users.noreply.github.com>
2022-11-18 21:35:19 +0000

    deploy: fb16beb4c5972acbae8ac7fa759e83e5746aba8c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.3-43-gd2b79f2',
-    LANGUAGE: 'None',
+    VERSION: 'v0.3-46-gfb16beb',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
